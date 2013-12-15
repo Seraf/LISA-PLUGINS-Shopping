c62aab53593030015e4a4d0dd048e87b9d744643
Julien Syx <julien.syx@gmail.com>
2013-12-15 10:17:27 +0100

    lisa instance passed to module can be None

--- modules/shopping.py
+++ modules/shopping.py
@@ -10,7 +10,7 @@ path = os.path.realpath(os.path.abspath(os.path.join(os.path.split(
 _ = translation = gettext.translation(domain='shopping', localedir=path, languages=[configuration['lang']]).ugettext
 
 class Shopping:
-    def __init__(self, lisa):
+    def __init__(self, lisa=None):
         self.lisa = lisa
         self.configuration_lisa = configuration
         self.mongo = MongoClient(self.configuration_lisa['database']['server'],
