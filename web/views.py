361880ef225ed31fdc8eb3aadb61060432fe41a7
Julien Syx <julien.syx@gmail.com>
2013-12-17 23:11:59 +0100

    Adding web + api part

---
+++ web/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.template import Context, loader
+from django.http import HttpResponse
+
+from django.contrib.auth.decorators import login_required
+import os,json
+
+from weblisa.utils import method_restricted_to, is_ajax
+from weblisa.settings import LISA_PATH
+
+from Shopping.modules.shopping import Shopping
+
+# Template system will be fixed with Django 1.7. Each plugin will be able to have his own templates
+@login_required()
+def index(request):
+    content = Shopping().list(jsonInput={})
+
+    return render_to_response(os.path.abspath(os.path.dirname(__file__) + '/templates/shopping-index.html'),
+                              {
+                                  'content': 'test'
+                              },
+                              context_instance=RequestContext(request))
